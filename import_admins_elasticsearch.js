c7a7c7fd508299fa926403d24a8a80938c51d3ed
mikefab <mikefabrikant@gmail.com>
2016-11-06 17:08:05 -0500

    Error catch

--- import_admins_elasticsearch.js
+++ import_admins_elasticsearch.js
@@ -7,30 +7,37 @@ var geojson_dir = './data/' + geojson_src;
 var jsonfile = require('jsonfile');
 var files = fs.readdirSync(geojson_dir);
 var flag = 1;
-require('bluebird').map(files, function(file, i) {
-  console.log(file, i)
+var bluebird = require('bluebird');
+
+bluebird.map(files, function(file, i) {
+  console.log(file, i);
   return import_admins(file, i);
-}, {concurrency: 1});
+}, {concurrency: 1})
+.catch(function(err) {console.log(err);})
+.then(function() {console.log('Done with import of admins.');});
 
 function import_admins(file) {
   return new Promise(function(resolve, reject){
     var json = jsonfile.readFileSync(geojson_dir + '/' + file);
     var admin_level = file.match(/\d/)[0];
     bulk_es_insert(json.features, admin_level, file)
+    .catch(function(err) { return reject(err);})
     .then(function() {resolve();});
   });
 }
 
-function bulk_es_insert(records, admin_level, file) {
+function bulk_es_insert(records, admin_level) {
   return new Promise(function(resolve, reject) {
     require('bluebird').map(records, function(record, i) {
-      console.log(i, record.properties.ISO, admin_level)
+      console.log(i, record.properties.ISO, admin_level);
       return import_admin(record, admin_level);
-    }, {concurrency: 1}).then(function(){resolve();})
+    }, {concurrency: 1})
+    .catch(function(err) {return reject(err);})
+    .then(function() {resolve();});
   });
 }
 
-function import_admin(record, admin_level, file) {
+function import_admin(record, admin_level) {
   record.properties.admin_level = admin_level;
   record.properties.pub_src = geojson_src;
   return new Promise(function(resolve, reject) {
@@ -40,7 +47,7 @@ function import_admin(record, admin_level, file) {
     };
 
     if(flag === 1) {
-      es.index(options, record, function(err, data) {
+      es.index(options, record, function(err) {
         if (err) {
           console.log(err);
           // return reject(err);
