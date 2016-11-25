316dbbc1f167c8d1cd4cabc4d64b5ec90e1be685
mikefab <mikefabrikant@gmail.com>
2016-11-25 01:50:23 -0500

    don't upload to azure

--- app.js
+++ app.js
@@ -117,12 +117,12 @@ function download_shapefile_then_process(country_code){
  */
 function process_zip(country_code){
   return new Promise(function(resolve){
-    azure.upload_blob(container_name, country_code, zips_dir, 'zip')
-    .catch(function(err) { console.log(err);})
-    .then(function() {
+    // azure.upload_blob(container_name, country_code, zips_dir, 'zip')
+    // .catch(function(err) { console.log(err);})
+    // .then(function() {
       geo.unzip_and_geojson(country_code, zips_dir).then(function(){
         resolve();
       });
-    });
+    // });
   });
 }
