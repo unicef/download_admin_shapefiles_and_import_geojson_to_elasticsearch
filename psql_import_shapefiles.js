216abbf0fbe50d1575d954f9aa7e19bf5312f001
mikefab <mikefabrikant@gmail.com>
2017-03-09 18:06:09 -0500

    Only import most granular shapefile per country

--- psql_import_shapefiles.js
+++ psql_import_shapefiles.js
@@ -24,24 +24,32 @@ var geojson_dir = config.temp_storage + geojson_src;
 var files = fs.readdirSync(geojson_dir);
 var isos = Object.keys(
   files.reduce(
-  function(h,e) { var iso = e.match(/^[A-Z]{3}/)[0]; h[iso] = 1; return h;},
+  function(h,e) {
+    var iso = e.match(/^[A-Z]{3}/)[0];
+    h[iso] = 1;
+    return h;},
   {})
 );
 
-var needed_zeros = certain_admin_if_no_other(isos, [1, 2]);
-//var needed_ones = certain_admin_if_no_other(isos, [0, 2]);
-var wanted_files = files.filter(function(file) {
+var wanted_files = files.reduce(function(h, file) {
   var iso = file.match(/^[A-Z]{3}/)[0];
   var level = file.match(/\d/)[0];
-  return (level == 2 || level == 1 || needed_zeros[iso]);
-});
+  if (h[iso]) {
+    if (level > h[iso]) {
+      h[iso] = level;
+    }
+  } else {
+    h[iso] = level;
+  }
+  return h;
+}, {});
 
 // azure.create_storage_container(geojson_src)
 // .catch(console.log)
 // .then(function(){
-  bluebird.each(wanted_files, function(file, i) {
-    console.log(file, i);
-    return import_admins(file, i);
+  bluebird.each(Object.keys(wanted_files), function(country, i) {
+    console.log(country, wanted_files[country]);
+    return import_admins(country, wanted_files[country]);
   }, {concurrency: 1})
   .catch(console.log)
   .then(function() {
@@ -49,13 +57,10 @@ var wanted_files = files.filter(function(file) {
     process.exit();
   });
 
-function import_admins(file, i) {
-  var country_admin = file.split(/[\._]/g);
-  var country = country_admin[0].toLowerCase();
-  var admin_level = country_admin[1];
+function import_admins(country, admin_level) {
   return new Promise((resolve, reject) => {
-    var command = 'bash create_db.sh ' + country + ' ' + admin_level;
-    // console.log(command)
+    var command = 'bash create_db.sh ' + country.toLowerCase() + ' ' + admin_level + ' ' + geojson_src;
+    console.log(command)
     // resolve();
     exec(command, (err, stdout, stderr) => {
       if (err) {
@@ -67,20 +72,4 @@ function import_admins(file, i) {
       console.log(stdout);
     });
   });
-}
-
-function certain_admin_if_no_other(isos, ary_num) {
-  var solo_certain_admin_isos = {};
-  isos.forEach(function(iso) {
-    var good = 1;
-    ary_num.forEach(function(pre) {
-      if (fileExists(geojson_dir + '/' + iso + '_' + pre + '.json')) {
-        good = 0;
-      }
-    });
-    if (good) {
-      solo_certain_admin_isos[iso] = 1;
-    }
-  });
-  return solo_certain_admin_isos;
 }
